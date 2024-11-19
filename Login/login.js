74b8d121077ef144513dfd14d8f6feba0e313065
AwfulAsiful <awful872@gmail.com>
2024-11-19 13:21:08 +0600

    added popups for successful and unsuccesful message

--- Login/login.js
+++ Login/login.js
@@ -1,5 +1,6 @@
 const loginBtn = document.getElementById('login-btn')
-
+const successMsg = document.getElementById('succes-alert');
+const failMsg = document.getElementById('warining-alert')
 const getValue = (inputField) => {
     return document.getElementById(inputField).value;
 }
@@ -7,11 +8,43 @@ const reset = (inputField) => {
     return document.getElementById(inputField).value = "";
 }
 
-loginBtn.addEventListener('click',()=>{
-    let email=getValue('email');
-    let pass=getValue('password');
-    console.log(email,pass);
-    
+
+
+// Function to show the popup
+function showPopup() {
+    successMsg.classList.remove('hidden');
+    successMsg.classList.add('active');
+    setTimeout(() => {
+        successMsg.classList.remove('active');
+        successMsg.classList.add('hidden');
+    }, 3000); // Hide after 3 seconds
+}
+function showWarning() {
+    failMsg.classList.remove('hidden');
+    failMsg.classList.add('active');
+    setTimeout(() => {
+        failMsg.classList.remove('active');
+        failMsg.classList.add('hidden');
+    }, 3000); // Hide after 3 seconds
+}
+
+// Example trigger
+// Trigger the popup after 1 second
+
+loginBtn.addEventListener('click', () => {
+    let email = getValue('email');
+    let pass = getValue('password');
+    const loginEmail = 'admin@example.com';
+    const loginPass = 'admin';
+    if (email === loginEmail && pass === loginPass) {
+        // alert("Login Successful!!");
+        showPopup();
+    }
+    else {
+        showWarning();
+    }
+    // console.log(email,pass);
+
 })
 
 
